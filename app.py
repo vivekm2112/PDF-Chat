52f138935c3a3740330c6401c3715276bc7dca15
Vivek Mohan <90549664+vivekm2112@users.noreply.github.com>
2023-12-31 19:28:41 +0530

    multiple pdf chat
    till now I've made the GUI for this project along with adding API from openAI and huggungface. Thing that I'm stuck with is embedding will move forward to solve the issues I'm facing

---
+++ app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+
+def get_pdf_text(pdf_docs):
+   text=""
+   for pdf in  pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+         text += page.extract_text()
+         return text 
+
+def get_text_chunks(raw_text): 
+    text_splitter = CharacterTextSplitter(separator = "\n", chunk_size=1000, chunk_overlap=200, length_function=len)
+    chunks = text_splitter.split_text(raw_text)
+    return chunks
+
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with PDFs", page_icon=":books:")
+
+    st.header("Chat with PDFs :books:")
+    st.text_input("Ask a question about your documents:")
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        pdf_docs=st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                #get the pdf text
+                raw_text=get_pdf_text(pdf_docs) 
+                st.write(raw_text)  
+                #get the text chunks 
+                text_chunks=get_text_chunks(raw_text)
+                st.write(text_chunks)
+                #create vector store
+                
+
+        #chatgpt
+        # with st.sidebar:
+        #     st.subheader("Your documents")
+        #     uploaded_file = st.file_uploader("Upload your PDFs here and click on 'Process'")
+        #     if st.button("Process"):
+        #         # Add logic here to process the uploaded_file or perform other actions
+        #         st.write(f"Processing question: {question}")
+        #         st.write(f"Uploaded file: {uploaded_file}")
+
+if __name__=='__main__':
+    main()
